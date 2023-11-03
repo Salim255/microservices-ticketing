1b09286ab4ea49e72050506171111de1df459b6c
Salim <salim.hassan.welleat@gmail.com>
2023-11-03 01:14:02 +0100

    build: skaffold config file

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -8,6 +8,6 @@ app.use(json());
 
 app.listen(3000, () => {
   console.log("====================================");
-  console.log("Listening on port 3000!");
+  console.log("Listening on port 3000! !!!!!!");
   console.log("====================================");
 });
