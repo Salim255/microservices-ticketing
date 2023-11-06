f3a6142209827e97fbd632ceb7b767866194b57e
Salim <salim.hassan.welleat@gmail.com>
2023-11-06 23:14:06 +0100

    using que group

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -13,7 +13,10 @@ stan.on("connect", () => {
   console.log("====================================");
 
   //this is the object that we're going to listen to and we're going to receive some data through the subscription
-  const subscription = stan.subscribe("ticket:created");
+  const subscription = stan.subscribe(
+    "ticket:created",
+    "orders-service-Queue-group"
+  );
 
   subscription.on("message", (msg: Message) => {
     const data = msg.getData();
