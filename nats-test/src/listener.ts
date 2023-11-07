1a7e46c1af8a06ac57d84204d772a58c7d4a463a
Salim <salim.hassan.welleat@gmail.com>
2023-11-07 23:56:08 +0100

    Event Redelivery

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -18,14 +18,18 @@ stan.on("connect", () => {
   });
 
   //The option setManualAckMode(true) allow us to retreat received event in case something went wrong during saving data in DB for example
-  const options = stan.subscriptionOptions().setManualAckMode(true);
+  const options = stan
+    .subscriptionOptions()
+    .setManualAckMode(true)
+    .setDeliverAllAvailable();
 
   //this is the object that we're going to listen to and we're going to receive some data through the subscription
   const subscription = stan.subscribe(
     "ticket:created",
-    "orders-service-Queue-group",
+
     options
   );
+  //"orders-service-Queue-group",
 
   subscription.on("message", (msg: Message) => {
     const data = msg.getData();
