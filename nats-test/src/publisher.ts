0ecf540a536403be8bf7a9b173257c0cc75676b8
Salim <salim.hassan.welleat@gmail.com>
2023-11-09 00:55:42 +0100

    test: using the custom publisher

--- nats-test/src/publisher.ts
+++ nats-test/src/publisher.ts
@@ -1,4 +1,5 @@
 import nats from "node-nats-streaming";
+import { TicketCreatedPublisher } from "./events/ticket-created-publisher";
 console.clear();
 //Client to connect to our nats streaming server
 const stan = nats.connect("ticketing", "abc", {
@@ -11,17 +12,23 @@ stan.on("connect", () => {
   console.log("Publisher connected to NATS ");
   console.log("====================================");
 
+  const publisher = new TicketCreatedPublisher(stan);
+  publisher.publish({
+    id: "123",
+    title: "conecrt",
+    price: 50,
+  });
   //This is will be the information we want to share
   //In order to send data to NATS Streaming server we must convert the data to JSON format
-  const data = JSON.stringify({
+  /*  const data = JSON.stringify({
     id: "123",
     title: "concert",
     price: 20,
-  });
+  }); */
   //The publish function is how we publish some data, when we call publish we are going to passe in the name of the the channel or the subject we want to publish this information to, and we'll also pass in the data we want to share as well
-  stan.publish("ticket:created", data, () => {
+  /*  stan.publish("ticket:created", data, () => {
     console.log("====================================");
     console.log("Event published!!");
     console.log("====================================");
-  });
+  }); */
 });
