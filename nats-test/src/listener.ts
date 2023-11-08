646fb2610b9f6f1bf451138822b88953d387428b
Salim <salim.hassan.welleat@gmail.com>
2023-11-08 01:22:32 +0100

    Quick Refactor

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -1,6 +1,6 @@
-import nats, { Message, Stan } from "node-nats-streaming";
+import nats from "node-nats-streaming";
 import { randomBytes } from "crypto";
-
+import { TicketCreatedListener } from "./events/ticket-created-listener";
 console.clear();
 //Client to connect to our nats streaming server
 const stan = nats.connect("ticketing", randomBytes(4).toString("hex"), {
@@ -47,56 +47,3 @@ stan.on("connect", () => {
 //Intercept terminate or interrupt request to our program, then we close the client gracefully, then the client will not receive event while it's closing
 process.on("SIGINT", () => stan.close());
 process.on("SIGTERM", () => stan.close());
-
-abstract class Listener {
-  abstract subject: string;
-  abstract queGroupName: string;
-  abstract onMessage(data: any, msg: Message): void;
-  private client: Stan;
-  protected ackWait = 5 * 1000;
-  constructor(client: Stan) {
-    this.client = client;
-  }
-
-  subscriptionOptions() {
-    return this.client
-      .subscriptionOptions()
-      .setDeliverAllAvailable()
-      .setManualAckMode(true)
-      .setAckWait(this.ackWait)
-      .setDurableName(this.queGroupName);
-  }
-
-  listen() {
-    const subscription = this.client.subscribe(
-      this.subject,
-      this.queGroupName,
-      this.subscriptionOptions()
-    );
-    subscription.on("message", (msg: Message) => {
-      console.log(`Message received: ${this.subject} / ${this.queGroupName}`);
-      const parsedData = this.parseMessage(msg);
-
-      this.onMessage(parsedData, msg);
-    });
-  }
-
-  parseMessage(msg: Message) {
-    const data = msg.getData();
-    return typeof data === "string"
-      ? JSON.parse(data)
-      : JSON.parse(data.toString("utf8"));
-  }
-}
-
-class TicketCreatedListener extends Listener {
-  subject = "ticket:created";
-  queGroupName = "payment-service";
-  onMessage(data: any, msg: Message): void {
-    console.log("====================================");
-    console.log("Event Data!", data);
-    console.log("====================================");
-
-    msg.ack();
-  }
-}
